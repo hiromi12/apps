1c408f5c09662a2adbcdd15360eea353f02e4076
hiromi12 <hhonda120901@gmail.com>
2023-12-25 16:19:45 -0600

    implement bim calculation

--- bmi-cpp/bim.cpp
+++ bmi-cpp/bim.cpp
@@ -5,3 +5,41 @@
  * @date 2023-12-24
  * 
  */
+
+#include <iostream>
+using namespace std;
+
+bool validator(double num) {
+  return num < 0 ? false : true;
+}
+double BMI(double height, double weight)
+{
+  return (weight / height) / height;
+}
+
+int main() {
+  double height = 0, weight = 0;
+  int counter = 0;
+  bool heightChecker = false;   // input validation
+  bool weightChecker = false;   // input validation
+
+  do {
+    if (counter > 1) {
+      cout << "Invalid input. Enter valid value." << endl;
+    }
+    cout << "Enter your height in meters: ";
+    cin >> height;
+    heightChecker = validator(height);
+    cout << "Enter your weight in kg: ";
+    cin >> weight;
+    weightChecker = validator(weight);
+
+    counter++;
+  } while(heightChecker == false || weightChecker == false);
+
+  double result = BMI(height, weight);
+
+  cout << "Your BMI is " << result << endl;
+
+  return 0;
+}
