bf0c16566daf468b5d0431df2abd02284ca5a3a0
hiromi12 <hhonda120901@gmail.com>
2023-12-25 16:28:34 -0600

    modify bim calculation

--- bmi-cpp/bim.cpp
+++ bmi-cpp/bim.cpp
@@ -3,43 +3,60 @@
  * @author Hiromi Honda
  * @brief BIM Calculator
  * @date 2023-12-24
- * 
+ *
  */
 
 #include <iostream>
 using namespace std;
 
-bool validator(double num) {
-  return num < 0 ? false : true;
-}
-double BMI(double height, double weight)
-{
-  return (weight / height) / height;
-}
+bool validator(double num);
+double BMI(double height, double weight);
 
-int main() {
+int main()
+{
   double height = 0, weight = 0;
   int counter = 0;
-  bool heightChecker = false;   // input validation
-  bool weightChecker = false;   // input validation
+  bool heightChecker = false; // input validation
+  bool weightChecker = false; // input validation
 
-  do {
-    if (counter > 1) {
+  do
+  {
+    if (counter >= 1)
+    {
       cout << "Invalid input. Enter valid value." << endl;
     }
     cout << "Enter your height in meters: ";
     cin >> height;
     heightChecker = validator(height);
+    counter++;
+  } while (heightChecker == false);
+
+  counter = 0;
+  do
+  {
+    if (counter >= 1)
+    {
+      cout << "Invalid input. Enter valid value." << endl;
+    }
     cout << "Enter your weight in kg: ";
     cin >> weight;
     weightChecker = validator(weight);
 
     counter++;
-  } while(heightChecker == false || weightChecker == false);
+  } while (weightChecker == false);
 
   double result = BMI(height, weight);
 
   cout << "Your BMI is " << result << endl;
 
   return 0;
+}
+
+bool validator(double num)
+{
+  return num < 0 ? false : true;
+}
+double BMI(double height, double weight)
+{
+  return (weight / height) / height;
 }
